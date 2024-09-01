d2aa49f863b22c25dd2cfd1ea1f082105456db24
wanziee <@ichwanalghifa@gmail.com>
2024-09-01 16:30:47 +0700

    Initial Commit

---
+++ latihan todolistApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  latihan todolistApp
+//
+//  Created by mohammad ichwan al ghifari on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ latihan todolistApp/latihan_todolistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  latihan_todolistAppApp.swift
+//  latihan todolistApp
+//
+//  Created by mohammad ichwan al ghifari on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct latihan_todolistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
